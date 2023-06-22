539987bd78f9291ad416e267189e677b64b38f93
tuanlac <peanuttfpg@gmail.com>
2023-06-22 16:44:58 +0700

    HTTPS=false in package.json

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,23 +6,12 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import "../components/assets/css/font.css";
 import theme from "../theme/theme";
 
-const Meta = () => {
-  return (
-    <head>
-      <meta http-equiv="Content-Security-Policy" content="default-src http:;"/>
-      <meta property="og:url" content="http://example.com" />
-    </head>
-  );
-}
-
 const queryClient = new QueryClient();
 function MyApp({ Component, pageProps }: any) {
   return (
     
     <AuthContextProvider>
-      <Meta />
       <UserContextProvider>
-        
           <ChakraProvider theme={theme}>
             <QueryClientProvider client={queryClient}>
                 <Component {...pageProps} />
