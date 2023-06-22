806eecf12a39792fd3d90a588648e6c4daedc454
tuanlac <peanuttfpg@gmail.com>
2023-06-22 15:09:36 +0700

    edit next-config.js

--- next.config.js
+++ next.config.js
@@ -1,29 +1,19 @@
-const { generateStaticParams } = require('next/dist/next-server/server/static-html');
-
 const nextConfig = {
-  reactStrictMode: true,
-  distDir: "build",
-  typescript: {
+    reactStrictMode: true,
+    distDir: "build",
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
     ignoreBuildErrors: true,
   },
   env: {
     NEXT_PUBLIC_BASE_URL: "http://18.143.102.131:8080",
+    // NEXT_PUBLIC_BEANOI_URL: "https://api.beanoi.com/api",
     NEXT_PUBLIC_TESTGZP_URL: "http://18.143.102.131:8080",
   },
   trailingSlash: true,
-  async generateBuildId() {
-    // Custom logic for generating build ID if needed
-    return 'your-custom-build-id';
-  },
-  async generateStaticParams() {
-    const staticParams = await generateStaticParams({
-      // Specify your page paths and their associated parameters here
-      '/': {},
-      // Add other pages/routes here
-    });
+}
 
-    return staticParams;
-  },
-};
-
-module.exports = nextConfig;
+module.exports = nextConfig
